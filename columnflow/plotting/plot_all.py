bc1a58e3d75d131537f96beb189b2b851c5bfc16
maadcoen <maarten.de.coen@cern.ch>
2024-06-21 15:44:01 +0200

    modularize 2d plotting function

--- columnflow/plotting/plot_all.py
+++ columnflow/plotting/plot_all.py
@@ -5,10 +5,17 @@ Example plot function.
 """
 
 from __future__ import annotations
+from unittest.mock import patch
+from functools import partial
 
 from columnflow.types import Sequence
 from columnflow.util import maybe_import, try_float
-from columnflow.plotting.plot_util import get_position, get_cms_label, FigAxesType
+from columnflow.plotting.plot_util import (
+    get_position,
+    get_cms_label,
+    FigAxesType,
+    fix_cbar_minor_ticks,
+)
 
 hist = maybe_import("hist")
 np = maybe_import("numpy")
@@ -334,3 +341,71 @@ def plot_all(
     plt.tight_layout()
 
     return fig, axs
+
+
+
+def make_plot_2d(
+    plot_config: dict,
+    style_config: dict,
+    figaxes: FigAxesType | None = None,
+    **kwargs,
+):
+    """
+    Function that makes a 2d plot based on two configuration dictionaries,
+    *plot_config* and *style_config*.
+
+    The *plot_config* contains the fields:
+    "hist": hist.Hist,
+    "kwargs": optional arguments to be passed to the plot_config["hist"].hist2d,
+    "cbar_kwargs": optional arguments to be passed to plt.colorbar (not sure if all will work),
+
+    The *style_config* expects fields (all optional):
+    "ax_cfg": dict,
+    "legend_cfg": dict,
+    "cms_label_cfg": dict,
+    "annotate_cfg": dict
+
+    :param plot_config: Dictionary with the histogram to be plotted and how (includes colorbar)
+    :param style_config: Dictionary that defines arguments on how to style the overall plot.
+    :param figaxes: tuple containing a figure and axes for the plots
+    :return: tuple of plot figure and axes
+    """
+
+    if figaxes is None:
+        # use CMS plotting style
+        plt.style.use(mplhep.style.CMS)
+        fig, ax = plt.subplots()
+    else:
+        fig, ax = figaxes
+        assert isinstance(ax, plt.Axes), f"expected one single axes but got {ax}"
+
+    # apply style_config
+    if ax_cfg := style_config.get("ax_cfg", {}):
+        for tickname in ["xticks", "yticks"]:
+            ticks = ax_cfg.pop(tickname)
+            for ticksize in ["major", "minor"]:
+                if subticks := ticks.get(ticksize, {}):
+                    getattr(ax, "set_" + tickname)(**subticks, minor=ticksize == "minor")
+        ax.set(**ax_cfg)
+
+    if "legend_cfg" in style_config:
+        ax.legend(**style_config["legend_cfg"])
+
+    # annotation of category label
+    if annotate_kwargs := style_config.get("annotate_cfg", {}):
+        ax.annotate(**annotate_kwargs)
+
+    if cms_label_kwargs := style_config.get("cms_label_cfg", {}):
+        mplhep.cms.label(ax=ax, **cms_label_kwargs)
+
+    # call plot method, patching the colorbar function
+    # called internally by mplhep to draw the extension symbols
+    with patch.object(plt, "colorbar", partial(plt.colorbar, **plot_config.get("cbar_kwargs", {}))):
+        plot_config["hist"].plot2d(ax=ax, **plot_config.get("kwargs", {}))
+
+    if plot_config.get("kwargs", {}).get("cbar", False):
+        # fix color bar minor ticks with SymLogNorm
+        fix_cbar_minor_ticks(ax.collections[0].colorbar)
+
+    fig.tight_layout()
+    return fig, (ax,)
