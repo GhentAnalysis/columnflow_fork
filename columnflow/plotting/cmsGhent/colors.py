89bef618439a298234ca91899e9b686dfd9c6052
Jan van der Linden <jan.vdlinden@cern.ch>
2024-08-14 11:43:53 +0200

    indexing possible

--- columnflow/plotting/cmsGhent/colors.py
+++ columnflow/plotting/cmsGhent/colors.py
@@ -11,69 +11,92 @@ class DefaultColors:
 
     def rainbow(self):
         return RainbowColors()
-    
+
     def cat_six(self):
         return CATColorsSix()
 
     def cat_ten(self):
         return CATColorsTen()
-
+    
+    def __getitem__(self, i):
+        return self.colors[i]
 
 class CATColorsSix(DefaultColors):
     def __init__(self):
         super().__init__()
-        self.red =    "#e42536"
+        self.red = "#e42536"
         self.orange = "#f89c20"
-        self.blue =   "#5790fc"
+        self.blue = "#5790fc"
         self.purple = "#7a21dd"
         self.violet = "#964a8b"
-        self.grey =   "#9c9ca1"
+        self.grey = "#9c9ca1"
+        self.colors = [
+            self.red,
+            self.orange,
+            self.blue,
+            self.purple,
+            self.violet,
+            self.grey,
+        ]
+
 
 class CATColorsTen(DefaultColors):
     def __init__(self):
         super().__init__()
-        self.blue =        "#3f90da"
-        self.blue_light =  "#92dadd"
-        self.orange =      "#ffa90e"
+        self.blue = "#3f90da"
+        self.blue_light = "#92dadd"
+        self.orange = "#ffa90e"
         self.orange_dark = "#e76300"
-        self.red =         "#bd1f01"
-        self.purple =      "#832db6"
-        self.brown =       "#a96b59"
-        self.ochre =       "#b9ac70"
+        self.red = "#bd1f01"
+        self.purple = "#832db6"
+        self.brown = "#a96b59"
+        self.ochre = "#b9ac70"
+        self.colors = [
+            self.blue,
+            self.blue_light,
+            self.orange,
+            self.orange_dark,
+            self.red,
+            self.purple,
+            self.brown,
+            self.ochre,
+        ]
+
 
 class RainbowColors(DefaultColors):
     def __init__(self):
         super().__init__()
-        self.purple =     "#d23be7"
-        self.blue =       "#4355db"
+        self.purple = "#d23be7"
+        self.blue = "#4355db"
         self.blue_light = "#34bbe6"
-        self.green =      "#49da9a"
-        self.lime =       "#a3e048"
-        self.yellow =     "#f7d038"
-        self.orange =     "#eb7532"
-        self.red =        "#e6261f"
+        self.green = "#49da9a"
+        self.lime = "#a3e048"
+        self.yellow = "#f7d038"
+        self.orange = "#eb7532"
+        self.red = "#e6261f"
         self.colors = [
-            self.purple,        
+            self.purple,
             self.blue,
             self.blue_light,
             self.green,
             self.lime,
             self.yellow,
             self.orange,
-            self.red
+            self.red,
         ]
+
 
 class PastelColors(DefaultColors):
     def __init__(self):
         super().__init__()
-        self.yellow =       "#ffa600"
-        self.orange =       "#ff843c"
-        self.orange_dark =  "#ff6562"
-        self.red =          "#fd5385"
+        self.yellow = "#ffa600"
+        self.orange = "#ff843c"
+        self.orange_dark = "#ff6562"
+        self.red = "#fd5385"
         self.violet_light = "#da52a2"
-        self.violet =       "#ab59b5"
-        self.purple =       "#6f5fba"
-        self.blue =         "#1761b0"
+        self.violet = "#ab59b5"
+        self.purple = "#6f5fba"
+        self.blue = "#1761b0"
         self.colors = [
             self.yellow,
             self.orange,
@@ -82,9 +105,6 @@ class PastelColors(DefaultColors):
             self.violet_light,
             self.violet,
             self.purple,
-            self.blue
+            self.blue,
         ]
-        
-
-
 
