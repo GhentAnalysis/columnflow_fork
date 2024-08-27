9b902d4db69514ef17783a864c934194695591fd
maadcoen <maarten.de.coen@cern.ch>
2024-08-27 16:49:27 +0200

    mistake in matching in case of multidim variables

--- columnflow/tasks/framework/base.py
+++ columnflow/tasks/framework/base.py
@@ -449,10 +449,15 @@ class AnalysisTask(BaseTask, law.SandboxTask):
                 # must eventually be a pattern, perform an object traversal
                 # make this also viable for multi-dim variables for plotting/histograms
                 name_parts = name.split("-")
-                all_objects = get_all_object_names()
                 # match all name parts
-                if all([law.util.multi_match(n, all_objects) for n in name_parts]):
-                    object_names.append(name)
+                matches = [[] for _ in name_parts]
+                for i, n in enumerate(name_parts):
+                    for _name in sorted(get_all_object_names()):
+                        if law.util.multi_match(_name, n):
+                            matches[i].append(_name)
+                for matches_pair in itertools.product(*matches):
+                    object_names.append("-".join(matches_pair))
+
         return law.util.make_unique(object_names)
 
     @classmethod
